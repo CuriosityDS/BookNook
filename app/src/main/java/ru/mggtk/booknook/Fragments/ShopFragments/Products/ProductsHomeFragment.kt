03d849e3ff95e2ce013f56db6925b797dc4baaa4
Sinxzc <nik.orig@mail.ru>
2023-12-05 17:42:53 +0300

    alpha 0.6

--- app/src/main/java/ru/mggtk/booknook/Fragments/ShopFragments/Products/ProductsHomeFragment.kt
+++ app/src/main/java/ru/mggtk/booknook/Fragments/ShopFragments/Products/ProductsHomeFragment.kt
@@ -45,16 +45,15 @@ class ProductsHomeFragment : Fragment() {
     private fun loadBooks(): List<Book> {
         // Временный пример:
         return listOf(
-            Book("Мастер и Маргарита", 599.99, "https://via.placeholder.com/150"),
-            Book("Преступление и наказание", 449.50, "https://via.placeholder.com/150"),
-            Book("Война и мир", 899.75, "https://via.placeholder.com/150"),
-            Book("1984", 349.25, "https://via.placeholder.com/150"),
-            Book("Маленький принц", 299.90, "https://via.placeholder.com/150"),
-            Book("Убить пересмешника", 539.80, "https://via.placeholder.com/150"),
-            Book("Мастер йоги", 189.60, "https://via.placeholder.com/150"),
-            Book("Морфий", 1359.99, "https://via.placeholder.com/150"),
-            Book("Норвежский лес", 499.00, "https://via.placeholder.com/150"),
-            Book("Позор Солнца", 699.99, "https://via.placeholder.com/150")
+            Book("Мастер и Маргарита", 599.99, "https://bidspirit-images.global.ssl.fastly.net/rusenamel/cloned-images/auction_73/lots/import_0/116/1/a_ignore_q_80_w_1000_c_limit_1.jpg"),
+            Book("Преступление и наказание", 449.50, "https://cdn1.ozone.ru/s3/multimedia-k/6717694592.jpg"),
+            Book("Война и мир", 899.75, "https://nauka.club/wp-content/auploads/914004/roman_tolstogo_voyna_mir.jpg"),
+            Book("1984", 349.25, "https://trikky.ru/wp-content/blogs.dir/1/files/2021/06/05/579f28c6-fbcd-4997-9973-544e63cbbb7b.jpeg"),
+            Book("Маленький принц", 299.90, "https://cdn1.ozone.ru/s3/multimedia-q/6053509382.jpg"),
+            Book("Убить пересмешника", 539.80, "https://cdn.img-gorod.ru/nomenclature/26/221/2622192.jpg"),
+            Book("Морфий", 1359.99, "https://cdn1.ozone.ru/s3/multimedia-6/6671523750.jpg"),
+            Book("Норвежский лес", 499.00, "https://фантазеры.рф/wa-data/public/shop/products/72/69/36972/images/83027/83027.750x0.jpg"),
+            Book("Позор Солнца", 699.99, "https://coollib.net/i/96/450496/cover.jpg")
         )
     }
 
